504e043fa83573fc5d2b984dfce4692043539f61
hfhs604 <admissions.hfhs.2026@gmail.com>
2026-09-09 23:13:27 +0530

    Update FeeManagementApp.jsx

--- hfhs-fees/src/components/FeeManagementApp.jsx
+++ hfhs-fees/src/components/FeeManagementApp.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { auth } from "../firebase/config";
 import {
-  getUserRole,
+  getUserRoleOnce,
   getAcademicSessions,
   createAcademicSession,
 } from "../firebase/feeService";
