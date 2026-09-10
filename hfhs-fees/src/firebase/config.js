a4ef882b63f674c76856eddd3a93aa1e0119dcc8
hfhs604 <admissions.hfhs.2026@gmail.com>
2026-09-10 09:47:07 +0530

    Update config.js

--- hfhs-fees/src/firebase/config.js
+++ hfhs-fees/src/firebase/config.js
@@ -14,7 +14,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = initializeFirestore(app, {
-  experimentalAutoDetectLongPolling: true,
+  experimentalForceLongPolling: true,
 });
 
 export const auth = getAuth(app);
